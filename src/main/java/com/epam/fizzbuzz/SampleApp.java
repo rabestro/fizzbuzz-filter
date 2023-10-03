a5c5b995bb0249eefe4ba6e91604f28a2774adfb
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-10-03 09:34:41 +0300

    Add SumOfNumbers.java and refactor Example.java
    Added a new feature SumOfNumbers that computes the sum of the numbers input by the user until '0' is entered. This provides a new function to the Fizzbuzz application, enabling more user interaction.
    
    Simultaneously, refactored the name of the class Example.java to SampleApp.java for better understanding of its functionality as it serves as a basic sample application. The method calling has also been updated according to the class name change.

--- src/main/java/com/epam/fizzbuzz/SampleApp.java
+++ src/main/java/com/epam/fizzbuzz/SampleApp.java
@@ -5,13 +5,13 @@ import java.util.stream.Stream;
 
 import static java.util.function.Predicate.not;
 
-public class Example {
+public class SampleApp {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
 
         Stream.generate(scanner::nextLine)
                 .takeWhile(not("exit"::equals))
-                .forEach(Example::process);
+                .forEach(SampleApp::process);
     }
 
     private static void process(String command) {
