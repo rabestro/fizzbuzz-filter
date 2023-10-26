461f20ec4831d6bb1298e51610390feb1384bea9
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-10-26 13:51:52 +0300

    Remove unused HappyNumberPredicate class, improve test readability
    The HappyNumberPredicate class was removed since it was never utilized in the project, improving code clarity.
    
    Also, the FizzBuzzFilterExerciseTest received a slight readability enhance thanks to updated test display names. This makes the purpose of individual tests more clear. Lastly, several test methods that seemed out of place were moved into the more fitting PredicateTest class, increasing overall coherence.

--- src/test/java/com/epam/fizzbuzz/FizzBuzzFilterExerciseTest.java
+++ src/test/java/com/epam/fizzbuzz/FizzBuzzFilterExerciseTest.java
@@ -3,11 +3,8 @@ package com.epam.fizzbuzz;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
 import java.util.function.IntPredicate;
-import java.util.function.Predicate;
 
-import static java.util.function.Predicate.not;
 import static java.util.stream.IntStream.rangeClosed;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -92,7 +89,7 @@ class FizzBuzzFilterExerciseTest {
 
 
     @Test
-    @DisplayName("Filter out numbers between numbers divisible by three and by five.")
+    @DisplayName("Bonus Task: Filter out numbers between numbers divisible by three and by five.")
     void numbers_between_a_numbers_divisible_by_three_and_by_five() {
         var numbers = rangeClosed(1, 20);
 
@@ -104,7 +101,7 @@ class FizzBuzzFilterExerciseTest {
     }
 
     @Test
-    @DisplayName("Filter out numbers between numbers divisible by five and by three.")
+    @DisplayName("Bonus Task: Filter out numbers between numbers divisible by five and by three.")
     void numbers_between_a_numbers_divisible_by_five_and_three() {
         var numbers = rangeClosed(1, 20);
 
@@ -135,47 +132,5 @@ class FizzBuzzFilterExerciseTest {
                 return result;
             };
         };
-    }
-
-
-    @Test
-    void the_length_of_the_sequence() {
-        var input = "1 7 9 0 5";
-
-        var count = new Scanner(input)
-                .tokens()
-                .takeWhile(not("0"::equals))
-                .count();
-
-        assertThat(count)
-                .as("The length of the sequence")
-                .isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("Filter out lines between [```java] and [```].")
-    void extract_all_java_code_snippets_from_markdown_document() {
-        var markdown = """
-                # Hello, World!
-                The following code snippet is written in Java:
-                ```java
-                System.out.println("Hello, World!");
-                ```
-                The following code snippet is written in Kotlin:
-                ```kotlin
-                println("Hello, World!")
-                ```
-                """;
-
-        Predicate<String> fizz = "```java"::equals;
-        Predicate<String> buzz = "```"::equals;
-
-        // TODO: Define the predicate
-        Predicate<String> fizzBuzz = s -> false;
-
-        assertThat(markdown.lines().filter(fizzBuzz))
-                .as("Java code snippets")
-                .containsExactly("""
-                        System.out.println("Hello, World!");""");
     }
 }
